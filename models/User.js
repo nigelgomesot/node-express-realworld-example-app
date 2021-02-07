116a2c8a98b16c9d26ebeb6c109e74f567f19785
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-02-06 19:03:31 -0600

    # bugfix...

--- models/User.js
+++ models/User.js
@@ -15,20 +15,20 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.plugin(uniqueValidator, { message: 'is already taken' })
 
-UserSchema.methods.setPassword = password => {
+UserSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex')
   this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
 }
 
-UserSchema.methods.validPassword = password => {
+UserSchema.methods.validPassword = function(password) {
   const hash =  crypto.pbkdf2(password, this.salt, 10000, 512, 'sha512').toString('hex')
 
   return this.hash === hash
 }
 
-UserSchema.methods.generateJWT = () => {
+UserSchema.methods.generateJWT = function() {
   const today = new Date()
-  let exp = new Date(today)
+  const exp = new Date(today)
   exp.setDate(today.getDate() + 60)
 
   return jwt.sign({
@@ -38,7 +38,7 @@ UserSchema.methods.generateJWT = () => {
   }, secret)
 }
 
-UserSchema.methods.toAuthJSON = () => {
+UserSchema.methods.toAuthJSON = function() {
   return {
     username: this.username,
     email: this.email,
