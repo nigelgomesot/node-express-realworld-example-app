359d121aa65f18605eb89e08c03a400ab53d2746
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-02-10 12:27:46 -0600

    # refactor...

--- routes/api/users.js
+++ routes/api/users.js
@@ -41,41 +41,39 @@ router.post('/users/login', (req, res, next) => {
 })
 
 
-// // Get user profile
-// router.get('/user', auth.required, (req, res, next) => {
-//   User.findById(req.payload.id).then(user => {
-//     if (!user)
-//       return res.sendStatus(401)
+// Get user profile
+router.get('/user', auth.required, (req, res, next) => {
+  User.findById(req.payload.id).then(user => {
+    if (!user)
+      return res.sendStatus(401)
 
-//     return res.json({ user: user.toAuthJSON() })
-//   }).catch(next)
-// })
+    return res.json({ user: user.toAuthJSON() })
+  }).catch(next)
+})
 
-// // Update user profile
-// router.put('/user', auth.required, (req, res, next) => {
-//   User.findById(req.payload.id).then(user => {
-//     if (!user)
-//       return res.sendStatus(401)
+// Update user profile
+router.put('/user', auth.required, (req, res, next) => {
+  User.findById(req.payload.id).then(user => {
+    if (!user)
+      return res.sendStatus(401)
 
-//     if (typeof req.body.user.username !== 'undefined')
-//       user.username = req.body.user.username
+    if (typeof req.body.user.username !== 'undefined')
+      user.username = req.body.user.username
 
-//     if (typeof req.body.user.email !== 'undefined')
-//       user.email = req.body.user.email
+    if (typeof req.body.user.email !== 'undefined')
+      user.email = req.body.user.email
 
-//     if (typeof req.body.user.password !== 'undefined')
-//       user.setPassword(req.body.user.password)
+    if (typeof req.body.user.password !== 'undefined')
+      user.setPassword(req.body.user.password)
 
-//     if (typeof req.body.user.bio !== 'undefined')
-//       user.bio = req.body.user.bio
+    if (typeof req.body.user.bio !== 'undefined')
+      user.bio = req.body.user.bio
 
-//     if (typeof req.body.user.image !== 'undefined')
-//       user.image = req.body.user.image
+    if (typeof req.body.user.image !== 'undefined')
+      user.image = req.body.user.image
 
-//     return user.save().then(() => res.json({ user: user.toAuthJSON }))
-//   }).catch(next)
-// })
-
-
+    return user.save().then(() => res.json({ user: user.toAuthJSON() }))
+  }).catch(next)
+})
 
 module.exports = router
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var secret =  require('../config').secret
 
 const getTokenFromHeader = req => {
   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') {
-    return eq.headers.authorization.split(' ')[1]
+    return req.headers.authorization.split(' ')[1]
   }
 
   return null
