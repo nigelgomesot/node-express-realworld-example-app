d271d86fea876ec86d8c691c5e7d394b967250b3
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-02-09 21:01:10 -0600

    # refactor...

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,18 +5,8 @@ var passport = require('passport')
 var User = mongoose.model('User')
 var auth = require('../auth')
 
-// Get user profile
-router.get('/user', auth.required, (req, res, next) => {
-  User.findById(req.payload.id).then(user => {
-    if (!user)
-      return res.sendStatus(401)
-
-    return res.json({ user: user.toAuthJSON() })
-  }).catch(next)
-})
-
 // Create user profile
-router.post('/auth/register', (req, res, next) => {
+router.post('/users', (req, res, next) => {
   const user = new User()
 
   user.username = req.body.user.username
@@ -28,40 +18,15 @@ router.post('/auth/register', (req, res, next) => {
   }).catch(next)
 })
 
-// Updat user profile
-router.put('/user', auth.required, (req, res, next) => {
-  User.findById(req.payload.id).then(user => {
-    if (!user)
-      return res.sendStatus(401)
-
-    if (typeof req.body.user.username !== 'undefined')
-      user.username = req.body.user.username
-
-    if (typeof req.body.user.email !== 'undefined')
-      user.email = req.body.user.email
-
-    if (typeof req.body.user.password !== 'undefined')
-      user.setPassword(req.body.user.password)
-
-    if (typeof req.body.user.bio !== 'undefined')
-      user.bio = req.body.user.bio
-
-    if (typeof req.body.user.image !== 'undefined')
-      user.image = req.body.user.image
-
-    return user.save().then(() => res.json({ user: user.toAuthJSON }))
-  }).catch(next)
-})
-
 // Login user
-router.post('/auth/login', (req, res, next) => {
+router.post('/users/login', (req, res, next) => {
   if (!req.body.user.email)
-    return res.status(422).json({ errors: {email: 'cannot be blank'} })
+    return res.status(422).json({errors: {email: 'cannot be blank'}})
 
   if (!req.body.user.password)
-    return res.status(422).json({ errors: {password: 'cannot be blank'} })
+    return res.status(422).json({errors: {password: 'cannot be blank'}})
 
-  passport.authenticate('local', { session: false }, function(err, user, info) {
+  passport.authenticate('local', {session: false}, function(err, user, info) {
     if (err)
       return next(err)
 
@@ -74,5 +39,43 @@ router.post('/auth/login', (req, res, next) => {
     }
   })(req, res, next)
 })
+
+
+// // Get user profile
+// router.get('/user', auth.required, (req, res, next) => {
+//   User.findById(req.payload.id).then(user => {
+//     if (!user)
+//       return res.sendStatus(401)
+
+//     return res.json({ user: user.toAuthJSON() })
+//   }).catch(next)
+// })
+
+// // Update user profile
+// router.put('/user', auth.required, (req, res, next) => {
+//   User.findById(req.payload.id).then(user => {
+//     if (!user)
+//       return res.sendStatus(401)
+
+//     if (typeof req.body.user.username !== 'undefined')
+//       user.username = req.body.user.username
+
+//     if (typeof req.body.user.email !== 'undefined')
+//       user.email = req.body.user.email
+
+//     if (typeof req.body.user.password !== 'undefined')
+//       user.setPassword(req.body.user.password)
+
+//     if (typeof req.body.user.bio !== 'undefined')
+//       user.bio = req.body.user.bio
+
+//     if (typeof req.body.user.image !== 'undefined')
+//       user.image = req.body.user.image
+
+//     return user.save().then(() => res.json({ user: user.toAuthJSON }))
+//   }).catch(next)
+// })
+
+
 
 module.exports = router
