eb9123a455ded772b52008cd1a2e0fdc6ec82d7e
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-02-13 14:14:41 -0600

    # articles WIP...

---
+++ models/Article.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const slug = require('slug')
+
+var ArticleSchema = new mongoose.Schema({
+  slug: {type: String, lowercase: true, unique: true},
+  title: String,
+  description: String,
+  body: String,
+  favoritesCount: {type: Number, default: 0},
+  tagList: [{type: String}],
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+}, {timestamps: true})
+
+ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'})
+
+ArticleSchema.pre('validate', next => {
+  if (!this.slug)
+    this.slugify()
+
+  next()
+})
+
+ArticleSchema.methods.slugify = function() {
+  this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6)).toString(36)
+}
+
+ArticleSchema.methods.toJSONFor = function(user) {
+  return {
+    slug: this.slug,
+    title: this.title,
+    description: this.description,
+    body: this.body,
+    favoritesCount: this.favoritesCount,
+    tagList: this.tagList,
+    author: this.author.toProfileJSONFor(user),
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt
+  }
+}
+
+mongoose.model('Article', ArticleSchema)
+
