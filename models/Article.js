5fa628136a2bfec05735e0c370bf9471c0d116a2
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-02-15 15:50:18 -0600

    # articles WIP...

--- models/Article.js
+++ models/Article.js
@@ -14,16 +14,16 @@ var ArticleSchema = new mongoose.Schema({
 
 ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'})
 
-ArticleSchema.pre('validate', next => {
+ArticleSchema.methods.slugify = function() {
+  this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36)
+}
+
+ArticleSchema.pre('validate', function(next) {
   if (!this.slug)
     this.slugify()
 
   next()
 })
-
-ArticleSchema.methods.slugify = function() {
-  this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6)).toString(36)
-}
 
 ArticleSchema.methods.toJSONFor = function(user) {
   return {
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -5,4 +5,48 @@ var User = mongoose.model('User')
 var Article = mongoose.model('Article')
 var auth = require('../auth')
 
+// Create article
+router.post('/', auth.required, (req, res, next) => {
+  User.findById(req.payload.id).then(user => {
+    if (!user)
+      return res.sendStatus(401)
+
+    const article = new Article(req.body.article)
+
+    article.author = user
+
+    return article.save().then(() => {
+      console.log(`article created for author: ${article.author}`)
+
+      return res.json({article: article.toJSONFor(user)})
+    })
+  }).catch(next)
+})
+
+router.param('article', (req, res, next, slug) => {
+  Article.findOne({slug: slug})
+    .populate('author')
+    .then(article => {
+      if (!article)
+        return res.sendStatus(404)
+
+        req.article = article
+        console.log(`article found for author: ${req.article.author}`)
+
+        return next()
+    }).catch(next)
+})
+
+// Get article
+router.get('/:article', auth.optional, (req, res, next) => {
+  Promise.all([
+    req.payload ? User.findById(req.payload.id) : null,
+    req.article.populate('author').execPopulate()
+  ]).then(results => {
+    const user = results[0]
+
+    return res.json({article: req.article.toJSONFor(user)})
+  }).catch(next)
+})
+
 module.exports = router
