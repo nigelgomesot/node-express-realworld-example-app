7b2d021136443e198910c1ef1faaab50d868fff2
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-02-16 10:16:35 -0600

    # articles WIP...

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -49,4 +49,26 @@ router.get('/:article', auth.optional, (req, res, next) => {
   }).catch(next)
 })
 
+// Update article
+
+router.put('/:article', auth.required, (req, res, next) => {
+  User.findById(req.payload.id).then(user => {
+    if (req.article.author._id.toString() === req.payload.id.toString()) {
+      if (typeof req.body.article.title !== 'undefined')
+        req.article.title = req.body.article.title
+
+      if (typeof req.body.article.description !== 'undefined')
+        req.article.description = req.body.article.description
+
+      if (typeof req.body.article.body !== 'undefined')
+        req.article.body = req.body.article.body
+
+      req.article.save().then(article => res.json({article: article.toJSONFor(user)})
+      ).catch(next)
+    } else {
+      return res.sendStatus(403)
+    }
+  })
+})
+
 module.exports = router
