cd761de8b8e5f08a7441c6c18a680d37e17e3f29
nigelgomesot <nigelgomes.ot@gmail.com>
2021-02-28 13:53:59 -0600

    # follow WIP...

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -14,7 +14,7 @@ router.param('username', (req, res, next, username) => {
   }).catch(next)
 })
 
-
+// Get user profile
 router.get('/:username', auth.optional, (req, res, next) => {
   if (req.payload) {
     User.findById(req.payload.id).then(user => {
@@ -25,6 +25,20 @@ router.get('/:username', auth.optional, (req, res, next) => {
     })
   } else
     return res.json({profile: req.profile.toProfileJSONFor(false)})
+})
+
+// Follow user
+router.post('/:username/follow', auth.required, (req, res, next) => {
+  const profileId = req.profile._id
+
+  User.findById(req.payload.id).then(user => {
+    if (!user)
+      return res.sendStatus(401)
+
+    return user.follow(profileId).then(() => {
+      return res.json({profile: req.profile.toProfileJSONFor(user)})
+    })
+  }).catch(next)
 })
 
 module.exports = router
